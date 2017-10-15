9654f7f6bce8c15b04443161f6b77a49411bf799
Marcus Noble <marcus@marcusnoble.co.uk>
2017-10-15 08:18:49 +0100

    Added cachebusting to SW

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,10 +4,22 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(fetchAndCache(event));
 });
 
+function cacheBust(request) {
+  var url = request.url;
+  if (url.indexOf(self.location.origin) >= 0) {
+    if (url.indexOf('.') < 0) {
+      url += `/`;
+    }
+    return `${url}?${Math.random()}`;
+  } else {
+    return request;
+  }
+}
+
 function fetchAndCache(event) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(event.request).then(response => {
-      var fetchResponse = fetch(event.request)
+      var fetchResponse = fetch(cacheBust(event.request))
         .then(function(networkResponse) {
           cache.put(event.request, networkResponse.clone());
           return networkResponse;
