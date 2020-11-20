8c3245d36d43f851dc0716095510e56a14b5fa30
Marcus Noble <averagemarcus@noreply.git.cluster.fun>
2020-11-20 08:08:51 +0000

    More bot filtering

--- filterRoutes.js
+++ filterRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 router.all('(/*)?/wp-admin/', function (req, res) {});
-router.all('*?/xmlrpc.php', function (req, res) {});
+router.all(/.*\.php$/, function (req, res) {});
 router.all('(/*)?/wp-includes/(*)?', function (req, res) {});
 router.all('/.git/*?', function (req, res) {});
-router.all('/wp-login.php', function (req, res) {});
+router.all('/.env', function (req, res) {});
+router.post('*', function (req, res) {});
+router.put('*', function (req, res) {});
+router.delete('*', function (req, res) {});
 
 module.exports = router
