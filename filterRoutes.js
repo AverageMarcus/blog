88e9ee5d333d47feb959c4c244eb3edda2882c83
Marcus Noble <github@marcusnoble.co.uk>
2021-06-21 04:22:36 +0100

    Filter more routes

--- filterRoutes.js
+++ filterRoutes.js
@@ -6,6 +6,8 @@ router.all('(/*)?/wp-admin/', blackHole);
 router.all(/.*\.php$/, blackHole);
 router.all(/.*\.aspx$/, blackHole);
 router.all(/.*\.gz$/, blackHole);
+router.all(/.*\.bz2$/, blackHole);
+router.all(/.*\.tar$/, blackHole);
 router.all('(/*)?/wp-includes/(*)?', blackHole);
 router.all('/.git/*?', blackHole);
 router.all('/.env', blackHole);
@@ -15,6 +17,9 @@ router.all('/admin(/.*)?', blackHole)
 router.all('/wordpress/', blackHole);
 router.all('/wp(2)?/', blackHole);
 router.all('/backup/', blackHole);
+router.all('/db-backup/', blackHole);
+router.all('/db_backup/', blackHole);
+router.all('/dbbackup/', blackHole);
 router.all('/bak/', blackHole);
 router.all(/.*\/mail\/config-.+\.xml/, blackHole);
 router.post('*', blackHole);
